1612f88d8e56e61d0beef9e6bd2293823523f0d2
Oleksii Lubynets <lubynets@lxbuild07.gsi.de>
2021-02-16 21:40:20 +0100

    add qna

---
+++ qna/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+LOGDIR=/lustre/cbm/users/$USER/qna/log
+mkdir -p $LOGDIR
+mkdir -p $LOGDIR/out
+mkdir -p $LOGDIR/error
+
+sbatch --job-name=QnA \
+        -t 07:20:00 \
+        --partition main \
+        --output=$LOGDIR/out/%j.out.log \
+        --error=$LOGDIR/error/%j.err.log \
+        -a 1-100 \
+        -- $PWD/batch_run.sh
