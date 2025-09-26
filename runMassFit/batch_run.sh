253095af61129d17d1a093718163a5b9fd2ce961
Oleksii Lubynets <O.Lubynets@gsi.de>
2025-09-26 19:23:55 +0200

    add runMassFit/

---
+++ runMassFit/batch_run.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+echo
+echo "Bash script started"
+date
+
+START_TIME=$SECONDS
+
+gcc --version
+cc --version
+
+source /lustre/alice/users/lubynets/soft/qa2/bin/.config.sh
+
+INDEX=${SLURM_ARRAY_TASK_ID}
+
+PROJECT_DIR=/lustre/alice/users/lubynets/QA
+
+MACRO_DIR=$PROJECT_DIR/macro
+
+OUTPUT_DIR=$PROJECT_DIR/outputs/runMassFit/set2
+WORK_DIR=$PROJECT_DIR/workdir
+LOG_DIR=$OUTPUT_DIR/log
+BATCH_LOG_DIR=$PROJECT_DIR/log
+
+mkdir -p $WORK_DIR/$INDEX
+mkdir -p $OUTPUT_DIR
+mkdir -p $LOG_DIR/jobs
+mkdir -p $LOG_DIR/out
+mkdir -p $LOG_DIR/error
+
+cd $WORK_DIR/$INDEX
+
+SCORES="0.00 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.10 0.11 0.12 0.13 0.14 0.15 0.16 0.17 0.18 0.19 0.20 0.21 0.22 0.23 0.24 0.25 0.26 0.27 0.28 0.29 0.30 0.31 0.32 0.33 0.34 0.35 0.36 0.37 0.38 0.39 0.40 0.41 0.42 0.43 0.44 0.45 0.46 0.47 0.48 0.49 0.50 0.51 0.52 0.53 0.54 0.55 0.56 0.57 0.58 0.59 0.60 0.61 0.62 0.63 0.64 0.65 0.66 0.67 0.68 0.69 0.70 0.71 0.72 0.73 0.74 0.75 0.76 0.77 0.78 0.79 0.80 0.81 0.82 0.83 0.84 0.85 0.86 0.87 0.88 0.89 0.90 0.91 0.92 0.93 0.94 0.95 0.96, 0.97, 0.98, 0.99"
+
+CONFIG_FILE=config_massfitter.json
+
+tarsig="NP"
+
+mkdir -p mInvFit mInvFit_Residuals RawYields_Lc configs
+for score in $SCORES
+do
+mkdir -p $score
+cd $score
+cp $MACRO_DIR/$CONFIG_FILE .
+sed -i "s/BDT_SCORE_TO_BE_REPLACED/$score/g" $CONFIG_FILE
+sed -i "s/TARGET_SIGNAL_TO_BE_REPLACED/$tarsig/g" $CONFIG_FILE
+sed -i "s/RANDOM_SEED_TO_BE_REPLACED/$INDEX/g" $CONFIG_FILE
+echo "start processing score = $score"
+runMassFitter $CONFIG_FILE  >& log_$INDEX.txt
+echo "finish processing score = $score"
+mv fileOut.root ../RawYields_Lc.${tarsig}gt${score}.root
+mv fileOut.pdf ../mInvFit.${tarsig}gt${score}.pdf
+mv fileOut_Residuals.pdf ../mInvFit_Residuals.${tarsig}gt${score}.pdf
+mv log_$INDEX.txt ..
+mv $CONFIG_FILE ../configs/$score.$CONFIG_FILE
+cd -
+rm -r $score
+done
+mv mInvFit.*pdf ./mInvFit
+mv mInvFit_Residuals*pdf ./mInvFit_Residuals
+mv RawYields_Lc*root ./RawYields_Lc
+tar rf RawYields_Lc.$INDEX.tar ./RawYields_Lc/RawYields_Lc.*
+
+mv log* $LOG_DIR/jobs
+mv $BATCH_LOG_DIR/out/$INDEX.out.log $LOG_DIR/out
+mv $BATCH_LOG_DIR/error/$INDEX.err.log $LOG_DIR/error
+
+cd ..
+mv $INDEX $OUTPUT_DIR
+
+mkdir -p $WORK_DIR/success
+cd $WORK_DIR/success
+touch index_${INDEX}
+
+echo
+echo "Bash script finished successfully"
+date
+
+FINISH_TIME=$SECONDS
+echo
+echo "elapsed time " $(($(($FINISH_TIME-$START_TIME))/60)) "m " $(($(($FINISH_TIME-$START_TIME))%60)) "s"
---
+++ runMassFit/run.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+source /lustre/alice/users/lubynets/batch/Helper.sh
+
+LOGDIR=/lustre/alice/users/$USER/QA/log
+mkdir -p $LOGDIR
+mkdir -p $LOGDIR/out
+mkdir -p $LOGDIR/error
+
+WORK_DIR=/lustre/alice/users/lubynets/QA/workdir
+
+A_LOW=1
+A_HIGH=100
+TIME_LIMIT=00:10:00
+
+NOT_COMPLETED=true
+ROUNDS=0
+A_HIGH=$(($A_HIGH+1))
+while [[ $NOT_COMPLETED = true && $ROUNDS < 5 ]]
+do
+date
+
+NOT_COMPLETED=false
+
+A=$(CreateJobsArray $A_LOW $A_HIGH $WORK_DIR/success)
+if [ ! -z $A ]; then
+NOT_COMPLETED=true
+fi
+
+if [ $NOT_COMPLETED = true ]
+then
+echo "Array " $A
+sbatch --job-name=MacroQA \
+       --wait \
+       -t $TIME_LIMIT \
+       --partition main \
+       --output=$LOGDIR/out/%a.out.log \
+       --error=$LOGDIR/error/%a.err.log \
+       -a $A \
+       -- $PWD/batch_run.sh
+fi
+ROUNDS=$(($ROUNDS+1))
+done
